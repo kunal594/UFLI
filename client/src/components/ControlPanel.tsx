4f9d93687f8b55af1e11df83d42ee54398d11e19
kunal594 <kunal.2201243cs@iiitbh.ac.in>
2025-07-01 19:49:23 +0530

    Update ControlPanel.tsx

--- client/src/components/ControlPanel.tsx
+++ client/src/components/ControlPanel.tsx
@@ -1,8 +1,17 @@
-export default function ControlPanel({ onPause, onRetry }: any) {
+type Props = {
+  onPause: () => void;
+  onRetry: () => void;
+};
+
+export default function ControlPanel({ onPause, onRetry }: Props) {
   return (
     <div className="flex gap-4 mt-4">
-      <button onClick={onPause} className="bg-yellow-400 px-3 py-1 rounded text-white">Pause</button>
-      <button onClick={onRetry} className="bg-green-500 px-3 py-1 rounded text-white">Retry</button>
+      <button onClick={onPause} className="bg-yellow-400 px-3 py-1 rounded text-white">
+        Pause
+      </button>
+      <button onClick={onRetry} className="bg-green-500 px-3 py-1 rounded text-white">
+        Retry
+      </button>
     </div>
   );
 }
